11ba18077f59f9102d720e35bfac8a50aedb0f95
whoacarolina <113652779+whoacarolina@users.noreply.github.com>
2022-11-04 11:07:14 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('myContainer')
+}
+
+function draw() {
+  background(75, 100 , 180);
+  // noStroke()
+  // rect(40, 40, 1400, 50, 10, 10, 10, 10)
+}
